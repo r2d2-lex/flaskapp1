efe47e344a070786b69e54232652d01d292a6d04
ksardas <r2d2-lex@yandex.ru>
2019-12-26 21:24:05 +0000

    add login and logout

---
+++ create_admin.py
@@ -0,0 +1,27 @@
+from getpass import getpass
+import sys
+
+from webapp import create_app
+from webapp.model import db, User
+
+app = create_app()
+
+with app.app_context():
+    username = input('Enter username:')
+
+    if User.query.filter(User.username == username).count():
+        print('User exists')
+        sys.exit(0)
+
+    password1 = getpass('Enter password: ')
+    password2 = getpass('Retype pass: ')
+    if not password1 == password2:
+        print('Password incorrect')
+        sys.exit(0)
+
+    new_user = User(username=username, role='admin')
+    new_user.set_password(password1)
+
+    db.session.add(new_user)
+    db.session.commit()
+    print('Создан пользователь с id {}'.format(new_user.id))
--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
-from webapp.model import db, News
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_login import LoginManager, login_user, logout_user
+from webapp.model import db, News, User
 
+from webapp.forms import LoginForm
 from webapp.weather import wether_by_city
 #from webapp.get_news import get_python_news
 
@@ -10,6 +12,14 @@ def create_app():
     app.config.from_pyfile('settings.py')
     db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+
     @app.route('/')
     def index():
         weather = wether_by_city(app.config['WEATHER_CITY'])
@@ -17,4 +27,30 @@ def create_app():
         news_list = News.query.order_by(News.published.desc()).all()
         print(news_list)
         return render_template('index.html',weather=weather,page_title=page_title,news_list=news_list)
+
+    @app.route('/login')
+    def login():
+        title = "Авторизация"
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
+
+    @app.route('/process-login', methods=['POST'])
+    def process_login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(username = form.username.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash('Вы вошли на сайт')
+                return redirect(url_for('index'))
+
+        flash('Неверное имя пользователя или пароль')
+        return redirect(url_for('login'))
+
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        flash('Вы разлогинились')
+        return redirect(url_for('index'))
+
     return app
