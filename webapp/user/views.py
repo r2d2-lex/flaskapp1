cdc3cc4bec91da38003ac3f5988e41f4133badf1
ksardas <r2d2-lex@yandex.ru>
2020-05-14 21:21:34 +0000

    Добавлена регистрация пользователя

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 from flask import render_template, flash, redirect, url_for
 from flask_login import login_user, logout_user, current_user
-from webapp.user.forms import LoginForm
+
+from webapp.model import db
+from webapp.user.forms import LoginForm, RegistrationForm
 from webapp.user.models import User
 
 blueprint = Blueprint('user', __name__, url_prefix='/user')
@@ -36,3 +38,27 @@ def logout():
     logout_user()
     flash('Вы разлогинились')
     return redirect(url_for('news.index'))
+
+
+@blueprint.route('/register')
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('news.index'))
+    form = RegistrationForm()
+    title = "Регистрация"
+    return render_template('user/registration.html', page_title=title, form=form)
+
+
+@blueprint.route('/process-reg', methods=['POST'])
+def process_reg():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        news_user = User(username=form.username.data, email=form.email.data, role='user')
+        news_user.set_password(form.password.data)
+        db.session.add(news_user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались!')
+        return redirect(url_for('user.login'))
+    print(form.errors)
+    flash('Пожалуйста исправьте ошибки в форме')
+    return redirect(url_for('user.register'))
