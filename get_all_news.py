fb84be14cbe40d1bd25a57f4bfbbf76c1cb74554
r2d2-lex <r2d2-lex@yandex.ru>
2020-08-13 19:28:44 +0500

    добавил граббинг контента статей+отображение статей

--- get_all_news.py
+++ get_all_news.py
@@ -1,9 +1,9 @@
 from webapp import create_app
-#from webapp.get_news import get_python_news
+#from webapp.get_pythonorg_news import get_python_news
 from webapp.news.parsers import habr
 
 
 app = create_app()
 with app.app_context():
-    #habr.get_habr_snipperts()
+    habr.get_habr_snipperts()
     habr.get_news_content()
--- webapp/news/views.py
+++ webapp/news/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, current_app
+from flask import abort, Blueprint, render_template, current_app
 from webapp.weather import wether_by_city
 from webapp.news.models import News
 
@@ -10,6 +10,15 @@ blueprint = Blueprint('news', __name__)
 def index():
     weather = wether_by_city(current_app.config['WEATHER_CITY'])
     page_title = "Прогноз погоды"
-    news_list = News.query.order_by(News.published.desc()).all()
+    news_list = News.query.filter(News.text.isnot(None)).order_by(News.published.desc()).all()
     print(news_list)
     return render_template('news/index.html', weather=weather, page_title=page_title, news_list=news_list)
+
+
+@blueprint.route('/news/<int:news_id>')
+def single_news(news_id):
+    my_news = News.query.filter(News.id == news_id).first()
+    if not my_news:
+        abort(404)
+
+    return render_template('news/view_one.html', page_title=my_news.title, news=my_news)
