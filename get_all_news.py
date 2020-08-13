91bd146f0b33cdf31f6439591d0ebe2efe2a6b46
r2d2-lex <r2d2-lex@yandex.ru>
2020-08-13 17:45:35 +0500

    Парсер статей Хабра по Python+ занесение в базу

--- get_all_news.py
+++ get_all_news.py
@@ -1,7 +1,9 @@
 from webapp import create_app
-from webapp.get_news import get_python_news
+#from webapp.get_news import get_python_news
+from webapp.news.parsers import habr
 
 
 app = create_app()
 with app.app_context():
-    get_python_news()
+    #habr.get_habr_snipperts()
+    habr.get_news_content()
